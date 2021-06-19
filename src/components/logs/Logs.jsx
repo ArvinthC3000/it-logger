8f2f44ba72ac1391b2690ef6de5a1a4e972f9e77
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-19 22:08:07 +0530

    PreLoader

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Preloader from '../layout/Preloader';
 import LogItem from './LogItem';
 
 const Logs = () => {
@@ -18,7 +19,7 @@ const Logs = () => {
     setLoading(false);
   };
 
-  if (loading) return <h4>Loading...</h4>;
+  if (loading) return <Preloader />;
   return (
     <ul className='collection with-header'>
       <li className='collection-header'>
