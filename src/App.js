19daaeba47d2d02aca762c334a4fa689adb2c1fe
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-19 21:41:13 +0530

    Search bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
-  return <div className='App'>My App</div>;
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
 };
 
 export default App;
