19401de4b2f1276eeea8a9324aca7487ee1dd0a0
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-19 22:05:08 +0530

    Log items component with Moments components

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import LogItem from './LogItem';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -17,16 +18,16 @@ const Logs = () => {
     setLoading(false);
   };
 
-  if (loading) <h4>loading...</h4>;
+  if (loading) return <h4>Loading...</h4>;
   return (
-    <ul className='collection-with-header'>
+    <ul className='collection with-header'>
       <li className='collection-header'>
         <h4 className='center'>System Logs</h4>
       </li>
       {!loading && !logs.length ? (
         <p className='center'>No logs to show...</p>
       ) : (
-        logs.map(log => <li> {log.message}</li>)
+        logs.map(log => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
