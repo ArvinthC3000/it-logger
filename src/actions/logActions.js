b7b47b3542473fe65b742c68c80a80d5f7c86f7f
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-20 20:49:26 +0530

    getLogs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './type';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOGS } from './type';
 
 // export const getLogs = () => {
 //   return async dispatch => {
@@ -24,6 +24,32 @@ export const getLogs = () => async dispatch => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Add logs from sersver
+export const addLogs = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOGS,
       payload: data,
     });
   } catch (err) {
