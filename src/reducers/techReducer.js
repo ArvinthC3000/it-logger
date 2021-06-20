3f8420c5b1c09e4fb291af1865bd1031cbefc5b3
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-20 22:46:43 +0530

    get tech reduer and state set up

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,26 @@
+import { GET_TECHS, SET_LOADING } from '../actions/type';
+
+const initialState = {
+  techs: null,
+  loading: false,
+  error: null,
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
