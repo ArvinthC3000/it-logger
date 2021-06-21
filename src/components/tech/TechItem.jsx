70e0f15df4b47b92a12b9493daa7dde4381a1482
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-21 21:58:35 +0530

    Delete tech bug

--- src/components/tech/TechItem.jsx
+++ src/components/tech/TechItem.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteTechs } from '../../actions/techAction';
+import M from 'materialize-css/dist/js/materialize.min';
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech: { firstName, lastName, id }, deleteTechs }) => {
+  const onDelete = () => {
+    deleteTechs(id);
+    M.toast({ html: 'Technician deleted' });
+  };
   return (
     <li className='collection-item'>
       <div>
-        {tech.firstName} {tech.lastName}
-        <a href='#!' className='secondary-content'>
+        {firstName} {lastName}
+        <a href='#!' onClick={onDelete} className='secondary-content'>
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
@@ -16,6 +23,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
   tech: PropTypes.object.isRequired,
+  deleteTechs: PropTypes.func.isRequired,
 };
 
-export default TechItem;
+export default connect(null, { deleteTechs })(TechItem);
